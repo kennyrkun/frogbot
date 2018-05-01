bad7e2980e9212790253ab2443c4cd0acdc4462d
Kenny McCormick <kennyrkun@users.noreply.github.com>
2018-05-01 08:52:24 -0500

    update frogbot.py
    rename Auth related things
    print command line arguments

--- frogbot.py
+++ frogbot.py
@@ -3,6 +3,7 @@ import asyncio
 import sys
 
 client = discord.Client()
+authTokenLocation = "./token.txt"
 
 @client.event
 async def on_ready():
@@ -16,6 +17,9 @@ async def on_message(message):
 	if message.content.startswith("!sins"):
 		await client.send_message(message.channel, "I heard Minecraft?")
 
-f = open("token.txt","r") #opens file with name of "test.txt"
-client.run(f.read())
+print("Starting with ", len(sys.argv), " arguments.")
+print(str(sys.argv])
+
+getAuthToken = open(authTokenLocation, "r") #opens file with name of "test.txt"
+client.run(getAuthToken.read())
 sys.exit()
