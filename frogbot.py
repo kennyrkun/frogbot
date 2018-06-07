a1651f612e1c18e9de2ed240fa919cef8b23d6ee
Kenny McCormick <keennymccormic+github@gmail.com>
2018-06-06 21:29:10 -0500

    added message log

--- frogbot.py
+++ frogbot.py
@@ -13,10 +13,12 @@ def getRandomFroggyQuote():
 
 @client.event
 async def on_ready():
-	print('logged in as ' + client.user.name + "(" + client.user.id + ")")
+	print("logged in as " + client.user.name + "(" + client.user.id + ")")
 
 @client.event
 async def on_message(message):
+	print("rec: " + message.content)
+
 	if message.content.startswith(botCommandPrefix + "sins"):
 		await client.send_message(message.channel, "I heard Minecraft?")
 	
