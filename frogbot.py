c3106db752fe08c71788b354bfd0776ed12e8429
Kenny McCormick <keennymccormic+github@gmail.com>
2018-06-07 22:05:56 -0500

    added help command.

--- frogbot.py
+++ frogbot.py
@@ -7,32 +7,32 @@ client = discord.Client()
 authTokenLocation = "./token.txt"
 frogQuotesLocation = "./resources/frogquotes.txt"
 botCommandPrefix = "!"
+frogHomeChannelID = "454470370817343488"
+botControllerRole = "Frog"
 
 def getRandomFroggyQuote():
-	#lines = open('file.txt').read().splitlines()
-	#myline = random.choice(lines)
 	return random.choice(open(frogQuotesLocation).read().splitlines())
 
 @client.event
 async def on_ready():
 	print("logged in as " + client.user.name + " (" + client.user.id + ")")
-	await client.send_message(client.get_channel("332310228890681345"), "yea what's up :white_check_mark:")
-	# TODO: have a configuration file set for bot, where this is read from.
+	await client.send_message(client.get_channel(frogHomeChannelID), "yea what's up? :white_check_mark:")
 
 @client.event
 async def on_message(message):
-	print("rec: " + message.content)
+	print(message.author.name + "#" + message.author.discriminator + ": " +  message.content)
 
-	# TODO: this
+	if message.content.startswith(botCommandPrefix + "help"):
+		await client.send_message(message.channel, "List of commands:\n\n`!help` - this dialog.\n`!fcuk` - responds with a random frog quote")
+
+#	TODO: this
 #	if message.content.startswith(botCommandPrefix + "sins"):
 #		await client.send_message(message.channel, "I heard Minecraft?")
 
 	if message.content.startswith(botCommandPrefix + "fcuk"):
 		await client.send_message(message.channel, getRandomFroggyQuote())
 
-#	if message.content.startswith(botCommandPrefix + "what"):
-#		await client.send_message(message.channel, "what the fuck do you want?")
-
+#	TODO: add this as a thing for admins
 #	if message.content.startswith(botCommandPrefix + "setgame"):
 #		await client.send_message(message.channel, "test")
 #		await client.change_presence(game=discord.Game(name='something goes here'))
