e8d2459f73b2b6d5a174daddf089b22ce6b3964d
Kenny McCormick <keennymccormic+github@gmail.com>
2018-12-17 20:26:26 -0600

    added !setplaying, per server settings
    per server settings can be created in the server_configurations folder. a standard configuration file with the server id as the name, and section. it follows the same structure of the bot configuration.
    !setplaying can update the playing text for the bot
    hopefully added the ability to work in any directory without changing strings
    added some error handlers, to keep from fucking up on dumb shit
    notify every server on startup
    added functions to make configs easier (don't know if they work)

---
+++ bot.bat
@@ -0,0 +1,1 @@
+python frogbot.py
--- frogbot.py
+++ frogbot.py
@@ -5,6 +5,10 @@ import random
 import configparser
 
 client = discord.Client()
+botConfig = configparser.ConfigParser()
+config = configparser.ConfigParser()
+
+botVersion = 6.0
 
 botAuthTokenPath = None
 botHomeChannelID = None
@@ -12,15 +16,16 @@ botControllerRole = None
 botCommandPrefix = None
 froggyQuotesPath = None
 
-def getConfiguration():
+workingPath = "./"
+
+def getBotConfiguration():
 	global botAuthTokenPath
 	global botHomeChannelID
 	global botControllerRole
 	global botCommandPrefix
 	global froggyQuotesPath
 
-	config = configparser.ConfigParser()
-	config.read('./home/pi/frogbot/bot.conf')
+	botConfig.read('./bot.conf')
 
 #	config['FROG_CONFIG'] = {'TokenPath': "./token.txt",
 #							'BotHomeChannelID': "454470370817343488",
@@ -29,18 +34,26 @@ def getConfiguration():
 #							'FroggyQuotesPath': "./resources/frogquotes.txt"
 #							}
 
-	botAuthTokenPath = config['FROG_CONFIG']['BotAuthTokenPath']
-	botHomeChannelID = config['FROG_CONFIG']['BotHomeChannelID']
-	botControllerRole = config['FROG_CONFIG']['BotControllerRole']
-	botCommandPrefix = config['FROG_CONFIG']['BotCommandPrefix']
-	froggyQuotesPath = config['FROG_CONFIG']['FroggyQuotesPath']
+	botAuthTokenPath = botConfig['FROG_CONFIG']['BotAuthTokenPath']
+	botHomeChannelID = botConfig['FROG_CONFIG']['BotHomeChannelID']
+	botControllerRole = botConfig['FROG_CONFIG']['BotControllerRole']
+	botCommandPrefix = botConfig['FROG_CONFIG']['BotCommandPrefix']
+	froggyQuotesPath = botConfig['FROG_CONFIG']['FroggyQuotesPath']
+
+def getBotConfigOption(option):
+	return botConfig['FROG_CONFIG'][option]
+
+def getServerConfigOption(serverID, option):
+	config = configparser.ConfigParser()
+	config.read("server_configurations/" + serverID + ".conf")
+	return config[serverID][option]
 
 def getRandomFroggyQuote():
 	return random.choice(open(froggyQuotesPath).read().splitlines())
-	
+
 def abilityCheck(message):
 		string = message.split()
-		
+
 		arg1 = "!roll"
 		arg2 = "20"
 		arg3 = "0"
@@ -49,6 +62,7 @@ def abilityCheck(message):
 		try:
 			arg3 = string[1]
 		except IndexError:
+			# arg3 default sot zero
 			print("no modifer was provided")
 		
 		# isn't it cool that I'm not duplicating the code here anymore?
@@ -56,38 +70,42 @@ def abilityCheck(message):
 		
 def rollDice(message):
 	string = message.split()
-	
+
 	if len(string) > 1:
 		dieSides = 20
-		
+
 		try:
 			dieSides = int(string[1])
 		except IndexError:
 			dieSides = 20
 		
 		modifier = 0
-		
+
 		try:
 			modifier = int(string[2])
 		except IndexError:
 			modifier = 0
-			
+
 		roll = random.randint(1, dieSides)
-		
+
 		naturalRoll = False
-		
+
 		# natural roll is highest number without modifier
 		# e.g. 20
 		if roll == dieSides:
 			naturalRoll = True
 		else:
-			roll += modifier
+			if modifier == 0:
+				if roll == 1:
+					naturalRoll = True
 			
+			roll += modifier
+
 			if roll > dieSides:
 				roll = dieSides
 
 		returnMessage = "nil"
-				
+
 		if naturalRoll:
 			returnMessage = str(roll) + " natural"
 		else:
@@ -97,43 +115,97 @@ def rollDice(message):
 				returnMessage = roll
 	else:
 		returnMessage = "please specify amount of sides"
-			
+
 	return returnMessage
+
+def percent():
+	return random.randint(0, 100)
 
 @client.event
 async def on_ready():
-	print("logged in as " + client.user.name + " (" + client.user.id + ")")
-	await client.send_message(client.get_channel(botHomeChannelID), "yea what's up? :white_check_mark:")
+#	print("logged in as " + client.user.name + " (" + client.user.id + ")")
+	await client.change_presence(game=discord.Game(name = "froggy being a fool", type = 3))
+	
+	try:
+		servers = list(client.servers)
+		for x in range(len(servers)):
+			channel = client.get_channel(getServerConfigOption(str(servers[x].id), 'BotHomeChannelID'))
+			await client.send_message(channel, "yea what's up? :white_check_mark: (v" + str(botVersion) + ")")
+	except Exception as exception:
+		await client.send_message(client.get_channel(botHomeChannelID), ":bangbang: Exception in on_ready: `" + exception.__class__.__name__ + '`:\n```' + str(exception) + "```")
+		return
+
+@client.event
+async def on_server_join(server):
+	try:
+		for c in server.channels:
+			if not c.type == discord.ChannelType.text:
+				if not c.permissions_for(server.me).send_messages:
+					continue
+				
+		await client.send_message(c, "yea what's up? :white_check_mark: (v" + str(botVersion) + ")")
+		
+	# TODO: include more information about server
+	except Exception as exception:
+		await client.send_message(client.get_channel(botHomeChannelID), ":bangbang: Exception in on_server_join: `" + exception.__class__.__name__ + '`:\n```' + str(exception) + "```" + "\nserver: " + server.id)
+		return
 
 @client.event
 async def on_message(message):
-	print(message.author.name + "#" + message.author.discriminator + ": " +  message.content)
+	try:
+		if message.content.startswith(botCommandPrefix + "help"):
+			await client.send_message(message.channel, "List of commands:\n\n`!help` - this dialog.\n`!fcuk` - responds with a random frog quote\n`!roll` - rolls a dice with the specified number of sides\narguments:\nsides: number of sides on the die\nmodifier: DnD modifier\n`!check` - DnD skill check\ninternally calls `!roll 20 [modifier]`\n`!percent` - random percent between 0 and 100.\n`!setplaying` - sets the playing status for the bot\n\nfrogbot version " + str(botVersion))
+			return
 
-	if message.content.startswith(botCommandPrefix + "help"):
-		await client.send_message(message.channel, "List of commands:\n\n`!help` - this dialog.\n`!fcuk` - responds with a random frog quote\n`!roll` - rolls a dice with the specified number of sides\narguments:\nsides: number of sides on the die\nmodifier: DnD modifier\n`!check` - DnD skill check\ninternally calls `!roll 20 [modifier]`")
+		if message.content.startswith(botCommandPrefix + "fcuk"):
+			await client.send_message(message.channel, getRandomFroggyQuote())
+			return
 
-	if message.content.startswith(botCommandPrefix + "fcuk"):
-		await client.send_message(message.channel, getRandomFroggyQuote())
-		
-#	TODO: this
-#	if message.content.startswith(botCommandPrefix + "sins"):
-#		await client.send_message(message.channel, "I heard Minecraft?")
+	#	TODO: this
+	#	if message.content.startswith(botCommandPrefix + "sins"):
+	#		await client.send_message(message.channel, "I heard Minecraft?")
+	#		return
 
-	if message.content.startswith(botCommandPrefix + "check"):
-		await client.send_message(message.channel, abilityCheck(message.content))
-		
-	if message.content.startswith(botCommandPrefix + "roll"):
-		await client.send_message(message.channel, rollDice(message.content))
+		if message.content.startswith(botCommandPrefix + "check"):
+			await client.send_message(message.channel, abilityCheck(message.content))
+			return
 
-#	TODO: add this as a thing for admins
-#	if message.content.startswith(botCommandPrefix + "setgame"):
-#		await client.send_message(message.channel, "test")
-#		await client.change_presence(game=discord.Game(name='something goes here'))
+		if message.content.startswith(botCommandPrefix + "roll"):
+			await client.send_message(message.channel, rollDice(message.content))
+			return
+
+		if message.content.startswith(botCommandPrefix + "percent"):
+			await client.send_message(message.channel, str(percent()) + "%")
+			return
+
+	#	TODO: restrict this to admins
+	#	TODO: !setwatching
+		if message.content.startswith(botCommandPrefix + "setplaying"):
+			# strip the first word
+			string = message.content.split(' ', 1) # skip first word ("!setplaying")
+			
+			try:
+				await client.send_message(message.channel, "aye")
+				gameName = string[1]
+
+				if gameName == "none":
+					await client.change_presence(game=discord.Game(name="froggy being a fool", type=3))
+				else:
+					await client.change_presence(game=discord.Game(name=gameName, type=3))
+			except IndexError:
+				await client.send_message(message.channel, "no game name provided")
+				
+			return
+	except Exception as exception:
+		await client.send_message(message.channel, ":bangbang: Exception in on_message: `" + exception.__class__.__name__ + '`:\n```' + str(exception) + "```")
+		return
 
 print("Starting with ", len(sys.argv), " arguments.")
 print(str(sys.argv))
 
-getConfiguration()
+os.chdir(workingPath)
+
+getBotConfiguration()
 getAuthToken = open(botAuthTokenPath, "r")
 client.run(getAuthToken.read())
 sys.exit()
